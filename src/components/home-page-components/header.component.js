f4afb43a69603cbe917d4e3dad651b7f49ab51ad
Sohaib Nehal <sohaib.nehal@ymail.com>
2020-11-14 23:18:57 +0500

    Removing extra modules

--- src/components/home-page-components/header.component.js
+++ src/components/home-page-components/header.component.js
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 
 function HeaderComponent() {
   const [isScroll, setIsScroll] = useState(false);
-  const { pathname, hash } = useLocation();
+  const { hash } = useLocation();
   const [show, setShow] = useState(false);
   const checkScroll = () => {
     if (window.scrollY > 120) {
@@ -19,30 +19,27 @@ function HeaderComponent() {
 
   const scrollToPosition = () => {
     // if not a hash link scroll to top
-    if(hash===''){
-      window.scrollTo(0, 0)
+    if (hash === '') {
+      window.scrollTo(0, 0);
     }
     // else scroll to id
-    else{
-        setTimeout(
-            () => {
-                const id = hash.replace('#', '');
-                const element = document.getElementById(id);
-                if (element) {
-                    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
-                }
-            },
-            0
-        );
+    else {
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }, 0);
     }
-  }
+  };
   useEffect(() => {
     document.addEventListener('scroll', checkScroll);
     return () => document.removeEventListener('scroll', checkScroll);
   });
-  useEffect(()=>{
+  useEffect(() => {
     scrollToPosition();
-  },[hash])
+  }, [hash]);
 
   return (
     <nav
