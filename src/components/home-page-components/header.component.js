d0aabed3e50ba117e089bcc6c33fa87b9f4b330a
Sohaib Nehal <sohaib.nehal@ymail.com>
2020-11-15 23:51:21 +0500

    Fixing the dependency issue

--- src/components/home-page-components/header.component.js
+++ src/components/home-page-components/header.component.js
@@ -17,22 +17,22 @@ function HeaderComponent() {
     setShow(!show);
   };
 
-  const scrollToPosition = () => {
-    // if not a hash link scroll to top
-    if (hash === '') {
-      window.scrollTo(0, 0);
-    }
-    // else scroll to id
-    else {
-      setTimeout(() => {
-        const id = hash.replace('#', '');
-        const element = document.getElementById(id);
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-      }, 0);
-    }
-  };
+  // const scrollToPosition = () => {
+  //   // if not a hash link scroll to top
+  //   if (hash === '') {
+  //     window.scrollTo(0, 0);
+  //   }
+  //   // else scroll to id
+  //   else {
+  //     setTimeout(() => {
+  //       const id = hash.replace('#', '');
+  //       const element = document.getElementById(id);
+  //       if (element) {
+  //         element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  //       }
+  //     }, 0);
+  //   }
+  // };
 
   useEffect(() => {
     document.addEventListener('scroll', checkScroll);
