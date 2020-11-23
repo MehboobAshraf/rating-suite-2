d981fef1b00e11e948d3eef40587560984c5e733
MehboobAshraf <mehboobashraf@hotmail.com>
2020-11-23 13:31:40 +0500

    update privacy policy  content

--- src/components/home-page-components/privacy-policy.component.js
+++ src/components/home-page-components/privacy-policy.component.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-const PrivacyPolicyComponent = (props) => {
+const PrivacyPolicyComponent = props => {
   return (
-    <section
-      className="section bg-light bg-privacy"
-      id="privacy-policy-section"
-    >
+    <section className="section bg-light bg-privacy" id="privacy-policy-section">
       <div className="container">
         <div className="row justify-content-center text-center">
           <div className="col-lg-12">
@@ -16,72 +13,141 @@ const PrivacyPolicyComponent = (props) => {
           </div>
         </div>
         <div className="row mt-4 justify-content-center text-center">
-          <i>Last updated: September 6, 2020.</i>
+          <i>Last updated: November 23, 2020.</i>
         </div>
         <div className="row mt-4 justify-content-center text-center">
           <p>
-            This privacy policy attempts to outline how Curious Concept websites
-            collect and use information from their users.
+            At Hensu LLC (“Ratingsuite”, "we," "our," or "us"), we take your privacy and the security of your information very seriously.
+            This Privacy Policy (“Privacy Policy”) covers the personal information we collect about you when you use our products or
+            services, or otherwise interact with us, including on our website at https://www.ratingsuite.com, and our related online and
+            offline offerings (collectively, the “Services”). This Privacy Policy is incorporated into, and is part of, our Terms of Service
+            available at https://www.ratingsuite.com/terms which govern your access to the Services.
+            <br />
+            <br />
+            This Privacy Policy (“Privacy Policy”) is designed to help you understand how we collect, use and share your personal
+            information and to assist you in exercising the privacy rights available to you. By using our Service, you consent to the
+            collection, transfer, processing, storage, disclosure and other uses of your information described in this Privacy Policy. If
+            you do not agree to the terms of this Policy, please do not use any of our services. Accordingly, when you use any of our
+            Services, you should check the date of this Policy and review any changes since you last reviewed the Policy.
           </p>
         </div>
         <div className="row mt-4 justify-content-center text-center">
-          <h5>LOGS</h5>
+          <h5>Information we collect</h5>
         </div>
         <div className="row mt-2 justify-content-center text-center">
           <p>
-            Like most other websites, Curious Concept websites collect
-            non-personal information about users whenever they access them.
-            These logs may include the browser name, the type of computer and
-            technical information about the means of connection to the site,
-            such as the operating system, the Internet service providers
-            utilized and other similar information.
+            We use third-party payment providers to process payments on the Services. We may receive information associated with your
+            payment information, such as billing address and transaction information (“Billing Information”), but we do not directly store
+            payment information on the Services.
+          </p>
+          <p>
+            When you create an account or otherwise use the Services, we collect information such as your name, company name, email address,
+            phone number, and role within your team or enterprise (“Personal Contact Information”).
+          </p>
+          <p>
+            As you use our Services, Ratingsuite may also collect information through the use of frequently used information-gathering
+            tools, such as cookies and Web beacons (“Website Navigational Information”). Website Navigational Information includes standard
+            information from your web browser (such as browser type and browser language), your Internet Protocol (“IP”) address, and the
+            actions you take on Ratingsuite’s website (such as web pages viewed and links clicked). <br />
+            <br />
+            We may use third parties, such as GoogleAnalytics or other analytics providers, to analyze traffic to our Site. They have their
+            own privacy policies addressing how they use the analytics and other information and we do not have access to, nor control over,
+            third parties’ use of cookies or other tracking technologies.
           </p>
         </div>
         <div className="row mt-4 justify-content-center text-center">
-          <h5>COOKIES</h5>
+          <h5>What we do with your information</h5>
+        </div>
+        <div className="row mt-2 justify-content-center text-center">
+          <p>Birdeye uses the information it collects in the following ways:</p>
         </div>
         <div className="row mt-2 justify-content-center text-center">
           <p>
-            Cookies are files with small amount of data which are sent to your
-            browser from a website and stored on your computer's hard drive.
-            Curious Concept websites uses them to to track preferences and AB
-            testing experiments. You can instruct your browser to refuse all
-            cookies or to indicate when a cookie is being sent.
+            We use this information primarily to administer our Services to you and provide you with updates and product announcements. Per
+            the Privacy and Conditions, we may use some of your information for marketing purposes.
+          </p>
+          <p>
+            Ratingsuite does not store any Billing Information on its servers. Instead, we use a payment provider, Stripe (www.stripe.com),
+            to store and process all payment related transactions.
+          </p>
+          <p>
+            Ratingsuite may use your Analytics Information in conjunction with an analytics service such as Google Analytics to monitor and
+            analyze use of the Services, to increase the Services’ functionality and user-friendliness, and to verify users have the
+            authorization required for the Services to process their requests.
           </p>
         </div>
         <div className="row mt-4 justify-content-center text-center">
-          <h5>DATA</h5>
+          <h5>Sharing & Disclosure of your information</h5>
         </div>
         <div className="row mt-2 justify-content-center text-center">
           <p>
-            With the exception of deliberately submitted contact requests and
-            error reports, Curious Concept websites do not collect or retain any
-            data submitted by users. Any contact requests or error reports
-            submitted will be kept confidential and deleted upon request.
+            Per our Privacy and Conditions, you agree to permit Ratingsuite to identify you as a customer and to use your name and/or logo
+            in Ratingsuite’s website and marketing materials.
+          </p>
+          <p>Ratingsuite does not sell, rent, or trade your private information to any third parties in any way.</p>
+          <p>
+            Ratingsuite may use certain trusted third party companies and individuals to help us provide, analyze, and improve the Services
+            (including, but not limited to, data storage, maintenance services, database management, web analytics, payment processing, and
+            improvement of the Services’ features). These third parties may have access to your information strictly for the purposes of
+            performing these tasks on our behalf and under obligations similar to those in this Privacy Policy.
+          </p>
+          <p>
+            We may disclose your non-private, aggregated, or otherwise non-personal information, such as usage statistics of our Services,
+            at our discretion.
           </p>
         </div>
         <div className="row mt-4 justify-content-center text-center">
-          <h5>THIRD PARTY</h5>
+          <h5>Sharing & Disclosure of your information</h5>
         </div>
         <div className="row mt-2 justify-content-center text-center">
+          <p>We may share your personal information with the following categories of third parties:</p>
           <p>
-            Curious Concept websites use reputable third party vendors to
-            provide traffic analytics, security, and advertising services.
+            We may share any personal information we collect about you with our third-party service providers. The categories of service
+            providers to whom we entrust personal information include service providers for: (i) the provision of the Services; (ii) the
+            provision of information, products, and other services you have requested; (iii) marketing and advertising; (iv) payment and
+            transaction processing; (v) customer service activities; and (vi) the provision of IT and related services.
+          </p>
+          <p>
+            We may provide personal information to business partners to provide you with a product or service you have requested. We may
+            also provide personal information to business partners with whom we jointly offer products or services.
+          </p>
+          <p>We may share personal information with our affiliated entities.</p>
+          <p>
+            We do not use or share your information, including personal information, to advertise any third party’s products or services via
+            the Services. We may use and share your personal information with third-party advertising partners to market our own Services
+            and grow our Services’ user base, such as to provide targeted marketing about our own Services via third-party services. If you
+            prefer not to share your personal information with third-party advertising partners, you may follow the instructions below.
+          </p>
+          <p>We may share your personal information with other third parties, including other users, in the following circumstances:</p>
+          <p>
+            We may access, preserve, and disclose any information we store in association with you to external parties if we, in good faith,
+            believe doing so is required or appropriate to: (i) comply with law enforcement or national security requests and legal process,
+            such as a court order or subpoena; (ii) protect your, our, or others’ rights, property, or safety; (iii) enforce our policies or
+            contracts; (iv) collect amounts owed to us; or (v) assist with an investigation and prosecution of suspected or actual illegal
+            activity.
+          </p>
+          <p>
+            If we are involved in a merger, acquisition, financing due diligence, reorganization, bankruptcy, receivership, purchase or sale
+            of assets, or transition of service to another provider, then your information may be sold or transferred as part of such a
+            transaction, as permitted by law and/or contract.
           </p>
         </div>
         <div className="row mt-4 justify-content-center text-center">
-          <h5>SECURITY</h5>
+          <h5>Changes to our Privacy Policy</h5>
         </div>
         <div className="row mt-2 justify-content-center text-center">
           <p>
-            Additional security measures on Curious Concept websites are enabled
-            by CloudFlare. To achieve this CloudFlare, like Curious Concept
-            websites, collects non-personal information about users whenever
-            they access Curious Concept websites and/or other sites on the
-            Internet. These logs may include the browser name, the type of
-            computer and technical information about the means of connection to
-            the site, such as the operating system, the Internet service
-            providers utilized and other similar information.
+            We may revise this Privacy Policy from time to time at our sole discretion. If there are any material changes to this Privacy
+            Policy, we will notify you as required by applicable law. You understand and agree that you will be deemed to have accepted the
+            updated Privacy Policy if you continue to use the Services after the new Privacy Policy takes effect.
+          </p>
+        </div>
+        <div className="row mt-4 justify-content-center text-center">
+          <h5>Contact</h5>
+        </div>
+        <div className="row mt-2 justify-content-center text-center">
+          <p>
+          If you have any questions regarding this Policy, please contact us at <a href="mailto:support@ratingsuite.com">support@ratingsuite.com</a>
           </p>
         </div>
       </div>
