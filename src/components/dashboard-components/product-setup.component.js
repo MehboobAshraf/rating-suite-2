8d1ab1a08a2621a7d5ced612ca6c44ea3f313041
MehboobAshraf <mehboobashraf@hotmail.com>
2020-12-14 20:00:22 +0500

    minor style changes

--- src/components/dashboard-components/product-setup.component.js
+++ src/components/dashboard-components/product-setup.component.js
@@ -1,8 +1,8 @@
 // import { Row, Col, Form } from "reactstrap";
-import { Row, Col, Card, Form, Input, Button, Space, Select, Collapse } from "antd";
-import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { Row, Col, Card, Form, Input, Button, Space, Select, Collapse } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import ProductService from '../../services/product.service';
-import { useContext } from "react";
+import { useContext } from 'react';
 import ProductFormComponent from './product-form.component';
 
 import { AuthContext } from '../../context/AuthContext';
@@ -24,21 +24,20 @@ const ProductSetupComponent = () => {
   // }
   const layout = {
     labelCol: { span: 24, breakpoint: 'xs' },
-    wrapperCol: { span: 24, breakpoint: 'xs' },
-    
+    wrapperCol: { span: 24, breakpoint: 'xs' }
   };
 
   const createSandbox = async () => {
-    try{
-      const sandox = await ProductService.createSandbox()
-      console.log('sandox', sandox)
+    try {
+      const sandox = await ProductService.createSandbox();
+      console.log('sandox', sandox);
     } catch (e) {
-      console.log('e', e)
+      console.log('e', e);
     }
-  }
+  };
 
-  const onFinish = async(values) => {
-    console.log("Received values of form:1", values);
+  const onFinish = async values => {
+    console.log('Received values of form:1', values);
     // const res = await ProductService.create(values.product.product)
     // console.log('resposnse', res)
   };
@@ -46,35 +45,26 @@ const ProductSetupComponent = () => {
   let data = [
     {
       channels: [
-        {channel: 'testt', productUrl: 'https://test.com'},
-        {channel: 'testing', productUrl: 'https://testing.com'}
+        { channel: 'testt', productUrl: 'https://test.com' },
+        { channel: 'testing', productUrl: 'https://testing.com' }
       ],
-      product: "test"
+      product: 'test'
     },
     {
       channels: [
-        {channel: 'testt', productUrl: 'https://test.com'},
-        {channel: 'testing', productUrl: 'https://testing.com'}
+        { channel: 'testt', productUrl: 'https://test.com' },
+        { channel: 'testing', productUrl: 'https://testing.com' }
       ],
-      product: "test"
+      product: 'test'
     },
     {
       channels: [
-        {channel: 'testt', productUrl: 'https://test.com'},
-        {channel: 'testing', productUrl: 'https://testing.com'}
+        { channel: 'testt', productUrl: 'https://test.com' },
+        { channel: 'testing', productUrl: 'https://testing.com' }
       ],
-      product: "test"
+      product: 'test'
     }
-  ]
-
-  const genExtra = () => (
-    <PlusOutlined
-      onClick={event => {
-        // If you don't want click extra trigger collapse, you can prevent this:
-        event.stopPropagation();
-      }}
-    />
-  );
+  ];
 
   return (
     <div className="container-fluid">
@@ -86,14 +76,7 @@ const ProductSetupComponent = () => {
               <Card>
                 <Row>
                   <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                    <Form
-                      {...layout}
-                      name="dynamic_form_nest_item"
-                      onFinish={onFinish}
-                      autoComplete="off"
-                      layout="vertical"
-                    >
-
+                    <Form {...layout} name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off" layout="vertical">
                       <Row>
                         <Col xs={24} sm={24} lg={8}>
                           <Form.Item
@@ -102,8 +85,8 @@ const ProductSetupComponent = () => {
                             rules={[
                               {
                                 required: true,
-                                message: "Please input your product!",
-                              },
+                                message: 'Please input your product!'
+                              }
                             ]}
                           >
                             <Input />
@@ -120,62 +103,50 @@ const ProductSetupComponent = () => {
                                     <Space
                                       key={field.key}
                                       style={{
-                                        display: "flex",
-                                        marginBottom: 8,
+                                        display: 'flex',
+                                        marginBottom: 8
                                       }}
                                       align="start"
                                     >
-                                          <Form.Item
-                                            {...field}
-                                            name={[field.name, "channel"]}
-                                            fieldKey={[field.fieldKey, "channel"]}
-                                            label={`Channel ${index + 1}`}
-                                            rules={[
-                                              {
-                                                required: true,
-                                                message: "Missing Channel",
-                                              },
-                                            ]}
-                                          >
-                                          <Select
-                                            placeholder="Select Channel"
-                                            allowClear
-                                          >
-                                            <Select.Option value="ebay">
-                                              ebay
-                                            </Select.Option>
-                                            <Select.Option value="amazon">
-                                              Amazon
-                                            </Select.Option>
-                                            <Select.Option value="wallmart">
-                                              Wallmart
-                                            </Select.Option>
-                                          </Select>
-                                        </Form.Item>
-                                          <Form.Item
-                                            {...field}
-                                            name={[field.name, "productUrl"]}
-                                            fieldKey={[
-                                              field.fieldKey,
-                                              "productUrl",
-                                            ]}
-                                            label="Product URL"
-                                            rules={[
-                                              {
-                                                required: true,
-                                                message: "Missing Product Url",
-                                                type: "url",
-                                              },
-                                            ]}
-                                          >
-                                            <Input placeholder="Product URL" />
-                                          </Form.Item>
-                                          <MinusCircleOutlined
-                                            onClick={() => {
-                                              console.log('field', fields)
-                                              remove(field.name);
-                                            }}
-                                          />
+                                      <Form.Item
+                                        {...field}
+                                        name={[field.name, 'channel']}
+                                        fieldKey={[field.fieldKey, 'channel']}
+                                        label={`Channel ${index + 1}`}
+                                        rules={[
+                                          {
+                                            required: true,
+                                            message: 'Missing Channel'
+                                          }
+                                        ]}
+                                      >
+                                        <Select placeholder="Select Channel" allowClear>
+                                          <Select.Option value="ebay">ebay</Select.Option>
+                                          <Select.Option value="amazon">Amazon</Select.Option>
+                                          <Select.Option value="wallmart">Wallmart</Select.Option>
+                                        </Select>
+                                      </Form.Item>
+                                      <Form.Item
+                                        {...field}
+                                        name={[field.name, 'productUrl']}
+                                        fieldKey={[field.fieldKey, 'productUrl']}
+                                        label="Product URL"
+                                        rules={[
+                                          {
+                                            required: true,
+                                            message: 'Missing Product Url',
+                                            type: 'url'
+                                          }
+                                        ]}
+                                      >
+                                        <Input placeholder="Product URL" />
+                                      </Form.Item>
+                                      <MinusCircleOutlined
+                                        onClick={() => {
+                                          console.log('field', fields);
+                                          remove(field.name);
+                                        }}
+                                      />
                                     </Space>
                                   ))}
                                 </Col>
@@ -201,10 +172,7 @@ const ProductSetupComponent = () => {
                       </Form.List>
 
                       <Form.Item>
-                        <button
-                          type="submit"
-                          className="btn btn-custom btn-dashboard"
-                        >
+                        <button type="submit" className="btn btn-custom btn-dashboard">
                           Add Product
                         </button>
                       </Form.Item>
@@ -214,40 +182,40 @@ const ProductSetupComponent = () => {
               </Card>
             </Col>
           </Row>
-
-          { amplifyUser && amplifyUser.userStatus === 'NEW' ? <><h5 className="mt-5">Subscribe to sandbox</h5>
-          <Row>
-            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-              <Card>
-                <p>Sandbox has pre-loaded product reviews for you to explore Ratingsuite features. Try it free for 14 days</p>
-                <button
-                  type="btn"
-                  className="btn btn-custom btn-dashboard"
-                  onClick={createSandbox}
-                >
-                  Subscribe
-                </button>
-              </Card>
-            </Col>
-          </Row> </>: ''}
-        
         </div>
       </div>
       <div className="row">
         <div className="col text-left mt-5">
-        <h5>Your Products</h5>
-        <Collapse
-          accordion
-          defaultActiveKey={['1']}
-          onChange={() =>{}}
-        >
-          {data.map((product,idx) =>{
-            return(<Panel header={"Product " + (idx + 1 )} key={idx + 1} extra={genExtra()}>
-              <ProductFormComponent></ProductFormComponent>
-            </Panel>)
-          })}
-        </Collapse>
+          <h5>Your Products</h5>
+          <Collapse accordion defaultActiveKey={['1']} onChange={() => {}}>
+            {data.map((product, idx) => {
+              return (
+                <Panel header={'Product ' + (idx + 1)} key={idx + 1}>
+                  <ProductFormComponent></ProductFormComponent>
+                </Panel>
+              );
+            })}
+          </Collapse>
         </div>
+      </div>
+      <div className="text-left">
+        {amplifyUser && amplifyUser.userStatus === 'NEW' ? (
+          <>
+            <h5 className="mt-5">Subscribe to sandbox</h5>
+            <Row>
+              <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                <Card>
+                  <p>Sandbox has pre-loaded product reviews for you to explore Ratingsuite features. Try it free for 14 days</p>
+                  <button type="btn" className="btn btn-custom btn-dashboard" onClick={createSandbox}>
+                    Subscribe
+                  </button>
+                </Card>
+              </Col>
+            </Row>{' '}
+          </>
+        ) : (
+          ''
+        )}
       </div>
     </div>
   );
